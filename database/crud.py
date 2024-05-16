7d1578d6c0c228007f97aa41152a83d334700163
Aron Wiederkehr <aron.wiederkehr@gmail.com>
2024-05-16 19:01:06 +0200

    Implement FastAPI server and API endpoints

--- database/crud.py
+++ database/crud.py
@@ -4,3 +4,13 @@ from .database import engine
 
 def create_database():
     models.Base.metadata.create_all(bind=engine)
+
+def get_shops(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.ShopLocation).offset(skip).limit(limit).all()
+
+def create_shop(db: Session, shop: schemas.ShopLocationCreate):
+    db_shop = models.ShopLocation(**shop.dict())
+    db.add(db_shop)
+    db.commit()
+    db.refresh(db_shop)
+    return db_shop
--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from database import crud, models, schemas
+from database.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/shops/", response_model=schemas.ShopLocation)
+def create_shop(shop: schemas.ShopLocationCreate, db: Session = Depends(get_db)):
+    return crud.create_shop(db=db, shop=shop)
+
+@app.get("/shops/", response_model=list[schemas.ShopLocation])
+def read_shops(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    shops = crud.get_shops(db, skip=skip, limit=limit)
+    return shops
